fff261c7b62dc3f7db254c01959b3bced626cb32
giorava <giorava98@gmail.com>
2023-03-10 17:14:30 +0100

    updated container and dependency tests

--- test/boost_test.cpp
+++ test/boost_test.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 int main(){
-boost::array<int, 4> arr = {{1,2,3,4}};
-cout << "hi" << arr[0];
-return 0;
+    boost::array<int, 4> arr = {{1,2,3,4}};
+    cout << "hi" << arr[0];
+    return 0;
 }
